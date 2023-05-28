9774ee0e3b6a7ab55a2e7a0e573526bb07c81ba8
Ryan Dickey <ryantay87@gmail.com>
2023-05-28 17:36:44 -0500

    Testing out a navbar.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Nav from './Nav';
 import App from './App';
 
+ReactDOM.render(<Nav />, document.getElementById('nav'));
 ReactDOM.render(<App />, document.getElementById('root'));
