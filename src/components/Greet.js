3f860eb574558f239dd09df0d8ff3c6b7fedf522
Ryan Dickey <ryantay87@gmail.com>
2023-05-29 16:33:48 -0500

    Comments added for functional and class components

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react'; // React needed for JSX
+//import React, { Component } from 'react'; // { Component } needed for class components
 
 /*
 *
@@ -7,10 +8,15 @@ import React, { Component } from 'react';
     2. Absence of 'this' keyword
     3. Solution without using state
     4. Mainly responsible for the UI
+
+    In 2018, Hooks allows for use of state and other React features without writing a class.
 *
 */
 
-const Greet = () => <h1>Hello Kevin, Bryce, and Ryan!</h1>;
+// const Greet = () => <h1>Hello Kevin, Bryce, and Ryan!</h1>;
+const Greet = () => {
+    return <h1>Hello Kevin, Bryce, and Ryan!</h1>;
+}
 
 /*
 *
