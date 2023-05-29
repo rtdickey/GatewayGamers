768377acff2b4be110ee1e5d3047c84600e1a0c5
Ryan Dickey <ryantay87@gmail.com>
2023-05-29 16:13:39 -0500

    Understanding difference between functional and class components

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+/*
+*
+    Functional Components (aka: stateless, dumb, presentational)
+    1. Use these as much as possible
+    2. Absence of 'this' keyword
+    3. Solution without using state
+    4. Mainly responsible for the UI
+*
+*/
 
 const Greet = () => <h1>Hello Kevin, Bryce, and Ryan!</h1>;
+
+/*
+*
+    Class Components (aka: stateful, dumb, container)
+    1. More feature rich
+    2. Maintain their own private data (state)
+    3. Complex UI Logic
+    4. Provide lifecycle hooks
+*
+*/
+
+// class Greet extends Component {
+//     render() {
+//         return <h1>Hello Kevin, Bryce, and Ryan!</h1>;
+//     }
+// }
 
 export default Greet;
