9d114a0bffea27993fe37857817fde5665945f25
Ryan Dickey <ryantay87@gmail.com>
2023-05-30 22:12:03 -0500

    Removed tutorial code except counter component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import Greet from './components/Greet';
-// import Welcome from './components/Welcome';
-// import Message from './components/Message';
 import Counter from './components/Counter';
 
 function App() {
   return (
     <>
-      <Greet name="Kevin" heroName="Captain America">
-        <p>This is children props.</p>
-      </Greet>
-      <Greet name="Bryce" heroName="Spider-Man">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Ryan" heroName="Black Panther"></Greet>
-      <hr />
-      {/* <Welcome name="Kevin" heroName="Captain America">
-        <p>This is children props inside class component.</p>
-      </Welcome>
-      <Welcome name="Bryce" heroName="Spider-Man" />
-      <Welcome name="Ryan" heroName="Black Panther" /> */}
-      {/* <Message /> */}
       <Counter />
     </>
   )
