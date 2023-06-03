0438f02dfe3de26e0edf17c0b0ac3f4de2deaf84
Ryan Dickey <ryantay87@gmail.com>
2023-06-02 22:53:56 -0500

    Removed exclamation mark from site name.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from "react-dom";
 import { App } from "./App";
 
 ReactDOM.render(
-  <App siteName="Gateway Gamers!" />,
+  <App siteName="Gateway Gamers" />,
   document.getElementById("root")
 );
