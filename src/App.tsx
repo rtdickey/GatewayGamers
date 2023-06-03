93f404eef82534d632cbb52025a5851ce241e610
Ryan Dickey <ryantay87@gmail.com>
2023-06-02 21:05:09 -0500

    Installed bootstrap and setup site.scss file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import * as React from 'react';
+import * as React from "react";
+import "./scss/site.scss";
+import { ResponsiveNavbar } from "./components/ResponsiveNavbar";
+import { Container, Image } from "react-bootstrap";
 
 function App() {
   return (
-    <h1>Welcome to Gateway Gamers!</h1>
-  )
+    <>
+      <ResponsiveNavbar />
+      <Container>
+        <h1>Welcome to Gateway Gamers!</h1>
+        <Image src="/logo192.png" rounded />
+      </Container>
+    </>
+  );
 }
 
-export {App};
+export { App };
---
+++ src/components/ResponsiveNavbar.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Navbar, NavDropdown, Nav, Container } from "react-bootstrap";
+
+function ResponsiveNavbar() {
+  return (
+    <Navbar bg="primary" variant="dark" collapseOnSelect expand="sm">
+      <Container>
+        <Navbar.Brand href="/">Gateway Gamers</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-toggle" />
+        <Navbar.Collapse id="navbar-toggle" role="">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/link">Link</Nav.Link>
+            <NavDropdown title="Drop-down" id="nav-dropdown">
+              <NavDropdown.Item href="/action1">Action 1</NavDropdown.Item>
+              <NavDropdown.Item href="/action2">Action 2</NavDropdown.Item>
+              <NavDropdown.Item href="/action3">Action 3</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export { ResponsiveNavbar };
