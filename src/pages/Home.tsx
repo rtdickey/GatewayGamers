29a77f291d6c16991fce41a48f4f242bc42fb9ca
Ryan Dickey <ryantay87@gmail.com>
2023-06-17 00:12:16 -0500

    Removed Rows and Cols for now. Added Containers.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,25 @@
-import { Col, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Banner from "../components/Banner";
 import CardListGroup from "../components/CardListGroup";
-import FullPageLoader from "../components/FullPageLoader";
 
 const Home = () => {
   const trendyGames = [""];
   return (
     <>
-      <Row>
-        <Col>
-          <Banner>
-            <h1>Welcome to Gateway Gamers</h1>
-            <p>
-              We are a local game group, who gets together monthly and learn,
-              teach, and play many games!
-            </p>
-          </Banner>
-        </Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col>
-          <CardListGroup
-            title="The Hotness"
-            subtitle="The top 5 trending games of today"
-            items={trendyGames}
-          />
-        </Col>
-      </Row>
+      <Banner>
+        <h1>Welcome to Gateway Gamers</h1>
+        <p>
+          We are a local game group, who gets together monthly and learn, teach,
+          and play many games!
+        </p>
+      </Banner>
+      <Container>
+        <CardListGroup
+          title="The Hotness"
+          subtitle="The top 5 trending games of today"
+          items={trendyGames}
+        />
+      </Container>
     </>
   );
 };
