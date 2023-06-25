106c1b0b6e63355db11f71c373bc603af3e7a907
Ryan Dickey <ryantay87@gmail.com>
2023-06-25 17:08:00 -0500

    React Testing Library w/ Code Coverage

---
+++ src/components/Banner.test.tsx
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom";
+import { render, screen, cleanup } from "@testing-library/react";
+import Banner from "./Banner";
+
+afterEach(cleanup);
+
+test("banner renders", () => {
+  const result = render(<Banner />);
+  const someElement = result.container.querySelector("div");
+  expect(someElement).toBeInTheDocument();
+});
+
+test("banner renders with children", () => {
+  const children = "Hello";
+  render(<Banner>{children}</Banner>);
+  expect(screen.queryByText("Hello")).toBeInTheDocument();
+});
