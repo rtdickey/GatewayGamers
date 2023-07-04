b75892bf3d9345d8111649eb090a66482d4ec296
Ryan Dickey <ryantay87@gmail.com>
2023-07-04 09:14:42 -0500

    Moved handling submit to dashboard for game search

--- src/pages/protected/Dashboard.tsx
+++ src/pages/protected/Dashboard.tsx
@@ -1,24 +1,33 @@
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   GameSearchForm,
   GameSearchList,
 } from "../../components/boardgame-geek";
 
-const Dashboard = () => (
-  <>
-    <Row>
-      <Col sm={3}></Col>
-      <Col sm={6}>
-        <div className="mb-3">
-          <GameSearchForm />
-        </div>
-        <div>
-          <GameSearchList />
-        </div>
-      </Col>
-      <Col sm={3}></Col>
-    </Row>
-  </>
-);
+const Dashboard = () => {
+  const [searchResult, setSearchResult] = useState("");
+
+  const handleOnSubmit = (value: string) => {
+    console.log("We are on dashboard with value: ", value);
+  };
+
+  return (
+    <>
+      <Row>
+        <Col sm={3}></Col>
+        <Col sm={6}>
+          <div className="mb-3">
+            <GameSearchForm onSubmit={handleOnSubmit} />
+          </div>
+          <div>
+            <GameSearchList />
+          </div>
+        </Col>
+        <Col sm={3}></Col>
+      </Row>
+    </>
+  );
+};
 
 export default Dashboard;
