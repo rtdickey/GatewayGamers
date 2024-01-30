3ebb19b9cce73b0678bc0f337e53438db944c1dd
Ryan Dickey <ryantay87@gmail.com>
2024-01-29 23:06:28 -0600

    Added skeleton loading package.

--- src/components/boardgame-geek/HotGamesList.tsx
+++ src/components/boardgame-geek/HotGamesList.tsx
@@ -1,88 +1,91 @@
-import { Row, Col, Container, ListGroup, Fade, Card } from "react-bootstrap";
+import { Row, Col, Container, ListGroup, Card } from "react-bootstrap";
 import { useGetHotnessQuery } from "../../redux/services/getHotness";
 import { HotResponse } from "../../interfaces/BoardGameGeekAPI";
 import "./HotGamesList.css";
-import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HotGamesListGroup = () => {
-  const [show, setShow] = useState(false);
   const { data, error, isLoading } = useGetHotnessQuery();
-
-  useEffect(() => {
-    setShow(!isLoading);
-  }, [isLoading]);
   return (
-    <Fade in={show}>
-      <Container fluid className="bg-primary-alt px-3 pt-3">
-        <Container>
-          <Row className="pb-3">
-            <Col>
-              <span style={{ fontSize: "1.7rem", fontWeight: "bold" }}>
-                Trending Games
-              </span>
-              <br />
-              <span style={{ fontSize: "1.3rem", fontWeight: "bold" }}>
-                Most active Board Game Geek
-              </span>
-            </Col>
-          </Row>
-          <Row className="game__list">
-            <Col className="content">
-              <ListGroup horizontal variant="flush">
-                {error ? (
-                  <ListGroup.Item
-                    style={{ backgroundColor: "inherit" }}
-                    className="pb-4"
-                  >
-                    <span className="text-danger font-italic">
-                      Error while retrieving games.
-                    </span>
-                  </ListGroup.Item>
-                ) : data ? (
-                  data.map((game: HotResponse) => (
+    <>
+      {isLoading ? (
+        <Skeleton height={374} />
+      ) : (
+        <Container fluid className="bg-primary-alt px-3 pt-3">
+          <Container>
+            <Row className="pb-3">
+              <Col>
+                <span style={{ fontSize: "1.7rem", fontWeight: "bold" }}>
+                  Trending Games
+                </span>
+                <br />
+                <span style={{ fontSize: "1.3rem", fontWeight: "bold" }}>
+                  Most active Board Game Geek
+                </span>
+              </Col>
+            </Row>
+            <Row className="game__list">
+              <Col className="content">
+                <ListGroup horizontal variant="flush">
+                  {error ? (
                     <ListGroup.Item
-                      key={game.id}
-                      style={{ backgroundColor: "inherit", border: 0 }}
-                      onClick={() =>
-                        window.open(
-                          "https://boardgamegeek.com/boardgame/" + game.id,
-                          "_blank" // <- This is what makes it open in a new window.
-                        )
-                      }
+                      style={{ backgroundColor: "inherit" }}
+                      className="pb-4"
                     >
-                      <Card
-                        className="game__content"
-                        style={{ backgroundColor: "inherit", border: "none" }}
-                      >
-                        <Card.Body>
-                          <img
-                            src={game.thumbnail.value}
-                            alt={game.name.value + " game"}
-                            className="game__content__header me-2"
-                          />
-                          <p className="pt-2 px-4">
-                            {game.name.value}
-                            <br />
-                            <span
-                              style={{
-                                fontSize: "1rem",
-                                fontStyle: "italic",
-                              }}
-                            >
-                              published: {game.yearpublished.value}
-                            </span>
-                          </p>
-                        </Card.Body>
-                      </Card>
+                      <span className="text-danger font-italic">
+                        Error while retrieving games.
+                      </span>
                     </ListGroup.Item>
-                  ))
-                ) : null}
-              </ListGroup>
-            </Col>
-          </Row>
+                  ) : data ? (
+                    data.map((game: HotResponse) => (
+                      <ListGroup.Item
+                        key={game.id}
+                        style={{ backgroundColor: "inherit", border: 0 }}
+                        onClick={() =>
+                          window.open(
+                            "https://boardgamegeek.com/boardgame/" + game.id,
+                            "_blank" // <- This is what makes it open in a new window.
+                          )
+                        }
+                      >
+                        <Card
+                          className="game__content"
+                          style={{
+                            backgroundColor: "inherit",
+                            border: "none",
+                          }}
+                        >
+                          <Card.Body>
+                            <img
+                              src={game.thumbnail.value}
+                              alt={game.name.value + " game"}
+                              className="game__content__header me-2"
+                            />
+                            <p className="pt-2 px-4">
+                              {game.name.value}
+                              <br />
+                              <span
+                                style={{
+                                  fontSize: "1rem",
+                                  fontStyle: "italic",
+                                }}
+                              >
+                                published: {game.yearpublished.value}
+                              </span>
+                            </p>
+                          </Card.Body>
+                        </Card>
+                      </ListGroup.Item>
+                    ))
+                  ) : null}
+                </ListGroup>
+              </Col>
+            </Row>
+          </Container>
         </Container>
-      </Container>
-    </Fade>
+      )}
+    </>
   );
 };
 
